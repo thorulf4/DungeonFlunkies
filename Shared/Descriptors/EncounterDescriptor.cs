0a78b67b39222882b09ec8947ae97f943e2e5d3a
Thorulf Neustrup <thorulf@live.dk>
2022-07-08 00:37:34 +0200

    Commit missing files

---
+++ Shared/Descriptors/EncounterDescriptor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Descriptors
+{
+    public class EncounterDescriptor
+    {
+        public List<EntityDescriptor> Enemies { get; set; }
+        public List<EntityDescriptor> Allies { get; set; }
+        public DateTime TurnEnds { get; set; }
+
+        public EncounterDescriptor(List<EntityDescriptor> enemies, List<EntityDescriptor> allies, DateTime turnEnds)
+        {
+            Enemies = enemies;
+            Allies = allies;
+            TurnEnds = turnEnds;
+        }
+    }
+}
---
+++ Shared/Descriptors/IDescriptableHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Shared.Descriptors
+{
+    public static class IDescriptableHelpers
+    {
+        public static IEnumerable<T> GetDescriptors<T>(this IEnumerable<IDescriptable<T>> enumerable)
+        {
+            return enumerable.Select(u => u.GetDescriptor());
+        }
+    }
+}
